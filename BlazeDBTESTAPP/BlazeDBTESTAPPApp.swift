eb54d1c36d9e9694f960f1cc6d1d8b517ff623f0
Michael Danylchuk <mn_danylchuk@yahoo.com>
2026-04-09 23:41:02 -0700

    Initial Commit

---
+++ BlazeDBTESTAPP/BlazeDBTESTAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlazeDBTESTAPPApp.swift
+//  BlazeDBTESTAPP
+//
+//  Created by Michael Danylchuk on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BlazeDBTESTAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
